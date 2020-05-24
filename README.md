2a13c2b398a7d539515dd8d5d5526a55f9b395f2
Dom Marsic <dmars@protonmail.com>
2020-05-24 11:51:17 +0100

    Initial commit for Monty Hall simulation
    Monty Hall is a probability problem:
    https://en.wikipedia.org/wiki/Monty_Hall_problem
    
    The simulation runs a large number of iterations with or without
    switching to calculate the probability of success.

--- README.md
+++ README.md
@@ -1,2 +1,55 @@
-# montyhall
-Monty Hall problem simulation
+# Introduction
+
+This program simulates Monty Hall problem: https://en.wikipedia.org/wiki/Monty_Hall_problem.
+
+The candidate has a choice of three doors, one with a car and two with goats. The candidate chooses a door, at which time the host opens one of the other two doors and asks the candidate if they want to switch their choice.
+
+This is a probability problem that demonstrates that switching the door has a better outcome:
+
+* If the candidate doesn't switch the door, their success rate is 1/3.
+* If the candidate switches, their success rate is 1/2.
+
+# Run
+
+The simulation program is written in [Julia](https://julialang.org/) to allow me experiment with this language.
+
+## Candidate doesn't switch the door
+
+Runs 10,000 iterations without switching doors. The success rate is around 1/3 (33%).
+
+```
+$ julia montyhall.jl
+Running Monty hall with doors: [1, 2, 3] for 10000 times.
+Candidate switches doors: false. Success rate: 33.06
+```
+
+## Candidate switches the door
+
+Runs 10,000 iterations with switching doors. The success rate is around 1/2 (50%).
+
+```
+julia montyhall.jl --switch
+Running Monty hall with doors: [1, 2, 3] for 10000 times.
+Candidate switches doors: true. Success rate: 49.73
+```
+
+## Command-line options
+
+```
+-d, --doors DOORS     How many doors for Monty Hall problem. (type:
+                      Int64, default: 3)
+-n, --iterations ITERATIONS
+                      How many iterations to run (type: Int64,
+                      default: 10000)
+-s, --switch          The candidate switches doors when this option
+                      is set.
+```
+
+# Licence
+
+GNU v3. See LICENSE.
+
+# Author
+
+Domagoj Marsic, 2020
+Contact: dmars+github@protonmail.com
---
+++ montyhall.jl
@@ -0,0 +1,79 @@
+#------------------------------------------------------------------------------
+# montyhall - Simulation of Monty Hall problem
+# Copyright (C) 2020  Domagoj Marsic
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Contact:
+# https://github.com/dmarsic
+# <dmars+github@protonmail.com>
+#------------------------------------------------------------------------------
+
+using Printf
+using Logging
+using ArgParse
+
+logger = Logging.SimpleLogger(stdout, Logging.Warn)
+global_logger(logger)
+
+function parseCommandLineArgs()
+    s = ArgParseSettings()
+    @add_arg_table! s begin
+        "--doors", "-d"
+            help = "How many doors for Monty Hall problem."
+            arg_type = Int
+            default = 3
+        "--iterations", "-n"
+            help = "How many iterations to run"
+            arg_type = Int
+            default = 10000
+        "--switch", "-s"
+            help = "The candidate switches doors when this option is set."
+            action = :store_true
+    end
+    return parse_args(s)
+end
+
+function montyHall(doors, choice, switch)
+    car = rand(doors)
+    @info "Car is behind door $car"
+    goats = filter(x -> x != car, doors)
+    opens = rand(filter(x -> x != choice, goats))
+    @info "Candidate chose door $choice, Monty Hall opens door $opens"
+    switchDoor = rand(filter(x -> x != opens, doors))
+    if switch
+        choice = switchDoor
+        @info "Candidate switches! New choice is door $choice."
+    else
+        @info "Candidate sticks with door $choice."
+    end
+    return choice == car
+end
+
+function loopMonty(doors, attempts, candidateSwitches)
+    println("Running Monty hall with doors: $doors for $attempts times.")
+    hits = 0
+    for i in 1:attempts
+        hit = montyHall(doors, rand(doors), candidateSwitches)
+        @info "Hit: $hit"
+        hits += hit
+    end
+    successRate = hits / attempts * 100
+    @printf("Candidate switches doors: %s. Success rate: %.2f\n",
+        candidateSwitches, successRate)
+end
+
+args = parseCommandLineArgs()
+doors = collect(1:args["doors"])
+loopMonty(doors, args["iterations"], args["switch"])
